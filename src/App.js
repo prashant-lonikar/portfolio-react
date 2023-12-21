0989e82d267a2bc0914310e31f863f5e411ee107
Prashant Lonikar <prashant@climatealigned.co>
2023-12-21 21:06:48 +0530

    Added publications but padding issue during resizing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import About from "./components/About";
 import { Home } from "./components/Home";
 import Navbar from "./components/Navbar";
+import Publications from "./components/Publications";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Home />
       <About />
+      <Publications />
     </div>
   );
 }
